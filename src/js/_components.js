dfcbca80d42b8e7a24fb71dbc5ab001eb6a69405
artem <89ufisum@gmail.com>
2024-11-23 16:49:12 +0100

    mobile

--- src/js/_components.js
+++ src/js/_components.js
@@ -4,6 +4,8 @@ import './components/modals.js';
 import './components/slider.js';
 import './components/form.js';
 import '../js/functions/burger.js';
+import '../js/functions/tabs.js';
+import './components/simplebar.js';
 
 
 
